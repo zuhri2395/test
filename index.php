ca03c64634930e69965cc2b273e4b82d2dc8f1e1
Muhammad Zuhri Hanifullah <zuhri2395@gmail.com>
2017-04-28 09:54:02 +0700

    first

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html5>
+<html>
+    <head>
+        <title>Voice to Text</title>
+    </head>
+    <body>
+        <form action="speech1.php" method="POST" enctype="multipart/form-data">
+            <label for="voice">Input Voice File</label>
+            <input type="file" id="voice" name="voice">
+            <button type="submit">Submit</button>
+        </form>
+    </body>
+</html>
