af03eac7d0961059bcf46c589b8c58db4509a573
Muhammad Zuhri Hanifullah <zuhri2395@gmail.com>
2017-04-29 20:30:45 +0700

    change to async speech recognition

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 <html>
     <head>
         <title>Voice to Text</title>
+        <link href="http://hayageek.github.io/jQuery-Upload-File/4.0.10/uploadfile.css" rel="stylesheet">
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+        <script src="http://hayageek.github.io/jQuery-Upload-File/4.0.10/jquery.uploadfile.min.js"></script>
     </head>
     <body>
         <form action="speech1.php" method="POST" enctype="multipart/form-data">
@@ -9,5 +12,16 @@
             <input type="file" id="voice" name="voice">
             <button type="submit">Submit</button>
         </form>
+        <div id="fileuploader">Upload</div>
     </body>
+    <script>
+        $(document).ready(function() {
+	        $("#fileuploader").uploadFile({
+	        url:"speech1.php",
+	        fileName:"voice",
+            allowedTypes:"wav,mp3",
+            acceptFiles:"audio/*"
+	        });
+        });
+</script>
 </html>
--- speech1.php
+++ speech1.php
@@ -2,12 +2,14 @@
 require_once __DIR__ . '/vendor/autoload.php';
 use Google\Cloud\Speech\SpeechClient;
 use Google\Cloud\Storage\StorageClient;
+use Google\Cloud\Core\ExponentialBackoff;
+
 
 putenv('GOOGLE_APPLICATION_CREDENTIALS=Speech to text project-1c3b022df4fe.json');
 $serviceJson = json_decode(file_get_contents("Speech to text project-1c3b022df4fe.json"));
 
 $dir = __DIR__ . '/resources/';
-$allowedExt = array('wav');
+$allowedExt = array('wav', 'mp3');
 $uploadedFile = $_FILES['voice'];
 $results = '';
 
@@ -16,6 +18,9 @@ $finfo = finfo_open(FILEINFO_MIME_TYPE);
 $mime = finfo_file($finfo, $uploadedFile['tmp_name']);
 $filename = $dir . $ext[0] . '.flac';
 
+/**
+ * Convert into FLAC
+ **/
 $ffmpeg = FFMpeg\FFMpeg::create();
 $audio = $ffmpeg->open($uploadedFile['tmp_name']);
 $format = new FFMpeg\Format\Audio\Flac();
@@ -36,7 +41,23 @@ $speech = new SpeechClient([
 ]);
 $options = ['encoding' => 'FLAC'];
 $object = $bucket->object($ext[0]);
-$results = $speech->recognize($object, $options);
+$operation = $speech->beginRecognizeOperation($object, $options);
+$backoff = new ExponentialBackoff(10);
+$backoff->execute(function () use ($operation) {
+    print('Waiting for operation to complete' . PHP_EOL);
+    $operation->reload();
+    if (!$operation->isComplete()) {
+        throw new Exception('Job has not yet completed', 500);
+    }
+});
+if ($operation->isComplete()) {
+    if (empty($results = $operation->results())) {
+        $results = $operation->info();
+    }
+    print_r($results);
+}
 
-file_put_contents('transcript', serialize($results));
-header('location:result.php');
+// $results = $speech->recognize($object, $options);
+
+// file_put_contents('transcript', serialize($results));
+// header('location:result.php');
